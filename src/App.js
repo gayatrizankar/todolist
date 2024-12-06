c2347bef51a4f5da2daa35279415d88d7e7a0467
gayatrizankar <gayatrizankar30@gmail.com>
2024-12-06 11:30:41 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Todolist from './todolist'; // Import the Todolist component
+
+const App = () => {
+  return (
+    <div>
+      <Todolist /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todolist.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Todolist = () => {
+  return (
+    <div className="h-screen bg-cyan-950">
+      <div className="flex justify-center items-center h-full">
+        <h1 className="text-white text-4xl font-bold">My Todo List</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Todolist;
